fd00af4756e1865ef31950e508c355468979ca00
rvi01 <114779097+rvi01@users.noreply.github.com>
2023-04-27 14:06:17 -0400

    demo

--- src/components/ReactTooltip.tsx
+++
@@ -1,7 +0,0 @@
-import ReactTooltip from 'react-tooltip';
-<ReactTooltip id="my-tooltip" type="light" effect="float">
-  <p>Asset: {payload[0]?.payload.asset}</p>
-  <p>Risk Rating: {payload[0]?.payload.rating}</p>
-  <p>Risk Factors: {payload[0]?.payload.factors}</p>
-  <p>Year: {payload[0]?.payload.year}</p>
-</ReactTooltip>
