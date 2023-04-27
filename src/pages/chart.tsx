6b960c3995f77b0021d716c9a249e584e0aa9dba
rvi01 <114779097+rvi01@users.noreply.github.com>
2023-04-27 00:20:19 -0400

    fixing

--- src/pages/chart.tsx
+++ src/pages/chart.tsx
@@ -1,10 +1,13 @@
+import Link from "next/link";
 import LineGraph from "../components/LineChart";
 
 export default function Home() {
   return (
     <div>
       <h1>Risk Rating over time</h1>
-
+      <div>
+          <h2><Link href="/">Home</Link></h2>
+      </div>
       <LineGraph />
     </div>
   );
